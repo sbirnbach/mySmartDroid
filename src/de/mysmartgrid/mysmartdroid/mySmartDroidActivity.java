2ff34062ea9ead22b69f5eee3e5fb0454dd1a97c
paalsteek <stephan.platz@googlemail.com>
2010-07-23 01:01:30 +0200

    added menu to access settings

--- src/de/mysmartgrid/mysmartdroid/mySmartDroidActivity.java
+++ src/de/mysmartgrid/mysmartdroid/mySmartDroidActivity.java
@@ -6,6 +6,11 @@ import android.content.Intent;
 import android.widget.TabHost;
 import android.os.Bundle;
 
+//import some classes needed for the meny
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MenuInflater;
+
 public class mySmartDroidActivity extends TabActivity
 {
 	/** Called when the activity is first created. */
@@ -46,5 +51,27 @@ public class mySmartDroidActivity extends TabActivity
 
 		tabhost.setCurrentTabByTag("hour");
 
+	}
+
+	//inflate menu from res/main.xml
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	// Handle item selection
+	switch (item.getItemId()) {
+		case R.id.options:
+			return true;
+		case R.id.quit:
+			finish();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
 }
