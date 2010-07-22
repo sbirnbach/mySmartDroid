8c5eacbdd50eb4ac329f5dded0f25524b262664f
Simon Birnbach <simon@simon-birnbach.de>
2010-07-23 00:10:21 +0200

    Added tabs

---
+++ src/de/mysmartgrid/mysmartdroid/monthActivity.java
@@ -0,0 +1,19 @@
+package de.mysmartgrid.mysmartdroid;
+
+import android.app.Activity;
+import android.widget.TextView;
+import android.os.Bundle;
+
+public class monthActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+		TextView textview = new TextView(this);
+		textview.setText("This is the month tab.");
+        setContentView(textview);
+    }
+}
--- src/de/mysmartgrid/mysmartdroid/mySmartDroidActivity.java
+++ src/de/mysmartgrid/mysmartdroid/mySmartDroidActivity.java
@@ -1,9 +1,12 @@
 package de.mysmartgrid.mysmartdroid;
 
 import android.app.Activity;
+import android.app.TabActivity;
+import android.content.Intent;
+import android.widget.TabHost;
 import android.os.Bundle;
 
-public class mySmartDroidActivity extends Activity
+public class mySmartDroidActivity extends TabActivity
 {
     /** Called when the activity is first created. */
     @Override
@@ -11,5 +14,37 @@ public class mySmartDroidActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+		TabHost tabhost = getTabHost();
+		TabHost.TabSpec spec;
+		Intent intent;
+
+		intent = new Intent().setClass(this, hourActivity.class);
+		spec = tabhost.newTabSpec("hour").setIndicator("hour").setContent(intent);
+
+		tabhost.addTab(spec);
+
+		intent = new Intent().setClass(this, dayActivity.class);
+		spec = tabhost.newTabSpec("day").setIndicator("day").setContent(intent);
+
+		tabhost.addTab(spec);
+
+		intent = new Intent().setClass(this, monthActivity.class);
+		spec = tabhost.newTabSpec("month").setIndicator("month").setContent(intent);
+
+		tabhost.addTab(spec);
+
+		intent = new Intent().setClass(this, yearActivity.class);
+		spec = tabhost.newTabSpec("year").setIndicator("year").setContent(intent);
+
+		tabhost.addTab(spec);
+
+		intent = new Intent().setClass(this, nightActivity.class);
+		spec = tabhost.newTabSpec("night").setIndicator("night").setContent(intent);
+
+		tabhost.addTab(spec);
+
+		tabhost.setCurrentTabByTag("hour");
+
     }
 }
